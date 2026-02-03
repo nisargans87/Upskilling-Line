599bbc25b93cebb05cd797404affc4bf6612835d
NISARGA NS <nisargans011@gmail.com>
2026-02-03 14:48:37 +0530

    Update Special_Datatypes.py

--- Special_Datatypes.py
+++ Special_Datatypes.py
@@ -1,3 +1,5 @@
+# Tupple elements/items demonstration 
+# Run code to observe outputs 
 A = (2,3,4,5)
 print(A)
 B=(2.5 , 3.5 , 4.5 , 5,5 ) 
@@ -6,6 +8,45 @@ C =(0 , -5 , 10 , -20 )
 print(C)
 D = (2+5j , 3+5j , 7 )
 print(D)
+E = ("Nisarga" , 46 , "BCA" , 12+7j)
+print(E)
+print(type(A),type(B),type(C),type(D),type(E))
+# Sets elements/items demonstration 
+# Run code to observe outputs 
+Q = {'a','e','i','o','u'}
+print(Q)
+W = { 200,300,-400,500,-600}
+print(W)
+R = { 12+7j ,23+6j ,89+4j}
+print(R)
+I = { "Name" , "Age" , "College" , "Year"}
+print(I)
+T = { True}
+print(T)
+S = {None}
+print(S)
+print(type(S),type(T),type(W))
+# Lists elements/items demonstration 
+# Run code to observe outputs 
+J = [ 12,34,56]
+print(J)
+O = ["A" , "Z" , "a" , "b"]
+print(O)
+U = ["#" , "_" , "+"]
+print(U)
+G = ["why" , "what" , "how" , "when" ]
+print(G)
+print(type(O))
+print(type(G))
+
+
+
+
+
+     
+
+
+
 
 
 
