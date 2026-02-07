1076783a55726e7b98eb0073ed1ae15609c44854
NISARGA NS <nisargans011@gmail.com>
2026-02-07 09:16:45 +0530

    Enhance array examples and clarify set usage
    Added examples of tuple and empty array creation, and clarified array creation with sets.

--- Python_Code/Arrays_Functions_Methods.py
+++ Python_Code/Arrays_Functions_Methods.py
@@ -7,11 +7,13 @@ print(array[1],array[3])
 print(array[-1],array[3])
 print(array[0:3])
 print(array[0:1])
+
 # array creation and access through index numbers using tuples 
 array1=(22,66,24)
 print(array1[1])
 print(array1[1:2])
 print(array1[0:3])
+
 # array creation using sets 
 # TypeError: 'set' object is not subscriptable
 array2={13,14,15,16}
@@ -19,6 +21,7 @@ print(array2)
 print(array2[2])
 print("From two arrays:" , array[2] ,array1[3])
 print("From two arrays:" , array[2:3] ,array1[0:3])
+
 # statements ends here 
 # "Arrays can be created using tuples and lists but not with sets ". Find out the reasons . 
 # This program is used to creation of an arrays using lists and tuples and also case study about sets 
@@ -27,8 +30,13 @@ array3=["Nisarga" , 21 ,"Computer Science" , 9.26 , True ]
 print(array3)
 print(array3[2]) #Computer Science 
 
+# empty array creation 
+empty_ex = []
+print(empty_ex)
 
-#array creation using im
+# Creation of an array by importing an java array module 
+
+
 
 
 
