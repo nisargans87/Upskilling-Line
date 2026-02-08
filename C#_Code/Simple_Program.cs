6eae078eae1b3273c0663ec2c0513d5c625e77dd
NISARGA NS <nisargans011@gmail.com>
2026-02-08 20:01:57 +0530

    Update and rename First.cs to Simple_Program.cs

---
+++ C#_Code/Simple_Program.cs
@@ -0,0 +1,17 @@
+// Program to display string text to the console
+// FirstMethod.cs - Project Name 
+using System;
+// System is the library in C#
+namespace FirstProgram 
+{
+public class FirstProgram {
+    // Class declaration 
+    public static void Main(string[] args){
+        // Method declaration 
+        Console.WriteLine("My First Program in C#");
+        //  prints string using WriteLine to console
+    }
+    }
+}
+
+
