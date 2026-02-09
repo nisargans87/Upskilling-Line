19d655658534200f4a63a15012243f76e1dc3aa1
NISARGA NS <nisargans011@gmail.com>
2026-02-09 08:56:27 +0530

    Update Functions.py

--- Python_Code/Functions.py
+++ Python_Code/Functions.py
@@ -17,4 +17,24 @@ def A():
 A()
 ''' Output - First  Letter of the Alphabet is  <function A at 0x7904114bc220>
 <class 'function'> 133058377007648 '''
-    
+
+
+# Simple function usage 3
+def NumberCheck ():
+    num=int(input("Enter a number : "))
+    if num%2==0 :
+        print("The number is even i.e " , num )
+    else :
+        print("The number is odd i.e", num)
+        print(type(num),type(NumberCheck),type(print),type(input))
+NumberCheck()
+''' Output - '''
+
+# Simple function usage 4 
+def ListCheck ():
+    collection = [11,22,33,44]
+    print("The collection of number in the list is ", collection)
+    print(type(collection))
+ListCheck()
+
+
