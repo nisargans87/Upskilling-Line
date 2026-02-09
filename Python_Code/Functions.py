df7adca65f6243b1e13e5dd03dffd1aa97dc2f51
NISARGA NS <nisargans011@gmail.com>
2026-02-09 08:23:42 +0530

    Create Functions.py

---
+++ Python_Code/Functions.py
@@ -0,0 +1,13 @@
+# Declaration of Function and Calling Function without passing an arguments 
+# Simple Function
+def FunctionCreation():
+    print("Learnt , Applied and now Learning ! ")
+FunctionCreation()
+# indentation matters here too  
+''' Output - Learnt , Applied and now Learning ! '''
+def FunctionCreation():
+    print("Learnt , Applied and now Learning ! ")
+    FunctionCreation()
+  ''' Displays Blank Screen '''
+
+# 
