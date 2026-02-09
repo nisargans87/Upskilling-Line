e20a6c60166a7c598c9c98a5586c960283c49972
NISARGA NS <nisargans011@gmail.com>
2026-02-09 08:30:24 +0530

    Update Functions.py

--- Python_Code/Functions.py
+++ Python_Code/Functions.py
@@ -1,5 +1,5 @@
 # Declaration of Function and Calling Function without passing an arguments 
-# Simple Function
+# Simple Function usage 1 
 def FunctionCreation():
     print("Learnt , Applied and now Learning ! ")
 FunctionCreation()
@@ -10,4 +10,11 @@ def FunctionCreation():
     FunctionCreation()
   ''' Displays Blank Screen '''
 
-# 
+# Simple Function usage  2
+def A():
+    print("First  Letter of the Alphabet is " , A)
+    print(type(A),id(A))
+A()
+''' Output - First  Letter of the Alphabet is  <function A at 0x7904114bc220>
+<class 'function'> 133058377007648 '''
+    
