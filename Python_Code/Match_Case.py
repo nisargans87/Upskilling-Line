108ab4cd17626790783a6a042ee49867b3f3b34c
NISARGA NS <nisargans011@gmail.com>
2026-02-12 18:10:55 +0530

    Add match case example for login validation
    Demonstrate match case for email and password validation.

---
+++ Python_Code/Match_Case.py
@@ -0,0 +1,14 @@
+''' Match Case demonstration ( python3.10 )
+ Program consisting of Match Case , which checks exact match between the entered email, password with already set email and password respectively '''
+
+enter_email=str(input("Enter your registered email id --  "))
+enter_password = str(input("Enter password -- "))
+set_password = "nisarga"
+set_email = "nisargans011@gmail.com"
+match enter_password , enter_email :
+    case  "nisarga","nisargans011@gmail.com" :
+        print("Login Successfully !")
+    case _ :
+        print(" Invalid Password or email !, Try Again ")
+      # code ends here 
+      
